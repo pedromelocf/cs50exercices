7eacca03486dee9887ab7ed2a622d2f80b0a2a79
Pedro Melo <pedromelocf@yahoo.com>
2023-04-22 18:23:15 -0300

    Exercicio resolvido :)

--- module 2 - readability/readbility.c
+++ module 2 - readability/readbility.c
@@ -1,18 +1,28 @@
 #include <cs50.h>
-#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
+string Texto;
+int Letras = 0;
+int Palavras = 1;
+int Sentencas = 0;
+int ColemanLiau = 0;
+
+float calcular()
+{
+    float L = ((float) Letras / (float) Palavras) * 100;
+    float S = ((float) Sentencas / (float) Palavras) * 100;
+    float index = ((0.0588 * L) - (0.296 * S) - 15.8);
+    return index;
+}    
 
 int main(void)
 {
-    string Texto;
-    int Letras = 0;
-    int Palavras = 1;
-    int Sentencas = 0;
     Texto = get_string("Text:");
     for (int i = 0, n = strlen(Texto); i < n; i++)
     {
-        if ((Texto[i] >= 65 && Texto[i] <= 90)|| (Texto[i] >= 97 && Texto[i] <= 122))
+        if ((Texto[i] >= 65 && Texto[i] <= 90) || (Texto[i] >= 97 && Texto[i] <= 122))
         {
             Letras++;
         }
@@ -24,6 +34,19 @@ int main(void)
         {
             Sentencas++;
         }
-        printf ("%i,%i,%i\n", Letras, Palavras, Sentencas);
+    }
+    
+    ColemanLiau =  round(calcular());
+    if (ColemanLiau < 1)
+    {
+        printf("Before Grade 1\n");   
+    }
+    if (ColemanLiau > 1 && ColemanLiau < 16)
+    {
+        printf("Grade %i\n", ColemanLiau);
+    }
+    if (ColemanLiau > 16)
+    {
+        printf("Grade 16+\n");
     }
 }
