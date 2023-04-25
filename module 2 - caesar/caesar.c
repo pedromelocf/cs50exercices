1eb699e2d59c8461054a5b97b6fc9ddca20e2b72
Pedro Melo <pedromelocf@yahoo.com>
2023-04-25 18:38:25 -0300

    Criando solução. Retorno ainda incorreto.

--- module 2 - caesar/caesar.c
+++ module 2 - caesar/caesar.c
@@ -9,6 +9,7 @@ int main (int argc, string argv[])
     if (argc != 2 )
     {
         printf("Usage: ./caesar key");
+        return 1;
     }
     
     string key = argv[1];
@@ -17,10 +18,26 @@ int main (int argc, string argv[])
         if (!isdigit(key[i])) 
         {
             printf ("Usage: ./caesar key");
-        }
-        else 
-        {
-            printf("OK!");
+            return 1;
         }
     }
+    
+    int k = atoi(key);
+    
+    string plaintext = get_string("plaintext:");
+    int lenght = strlen(plaintext);
+    char ciphertext[lenght];
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        if (isalpha(plaintext[i]))
+        {
+            char caractere = isupper(plaintext[i]) ? 'A' : 'a' ;
+            ciphertext[i] = (caractere + k % 26) - caractere;
+        }
+        else
+        {
+            ciphertext[i] = plaintext[i];
+        }
+    }
+    printf("ciphertext:%s\n", ciphertext);
 }    
